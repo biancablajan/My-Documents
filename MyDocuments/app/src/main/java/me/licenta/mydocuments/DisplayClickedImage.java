ba389616a4e6bd4f1d9613cef6a0f62ee65dce6e
Bianca <bianca.blajan@fwfcompany.com>
2018-08-24 22:31:09 +0300

    REMOVE
    Remove button developed for displayed images. Will develop pinch zoom for images.

--- MyDocuments/app/src/main/java/me/licenta/mydocuments/DisplayClickedImage.java
+++ MyDocuments/app/src/main/java/me/licenta/mydocuments/DisplayClickedImage.java
@@ -1,9 +1,15 @@
 package me.licenta.mydocuments;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -15,9 +21,9 @@ public class DisplayClickedImage extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.displayclickedimage);
 
-        String imagePath = getIntent().getStringExtra("ClickedImagePath"); //get image path from post activity
+        final String imagePath = getIntent().getStringExtra("ClickedImagePath"); //get image path from post activity
         if (imagePath != null) {
-            File imgFile = new File(imagePath);
+             File imgFile = new File(imagePath);
             if (imgFile.exists()) {
 
                 Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
@@ -28,8 +34,23 @@ public class DisplayClickedImage extends Activity {
             }
 
         }
-
-
+        //Removes the displayed file and returns to previous Activity (View)
+        final Button buttonRemove = (Button) findViewById(R.id.buttonRemove);
+        buttonRemove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        File file = new File(imagePath);
+                        file.delete();
+                        DisplayClickedImage.super.onBackPressed();
+                    }
+                },3000);
+            }
+        });
 
     }
+
+
 }

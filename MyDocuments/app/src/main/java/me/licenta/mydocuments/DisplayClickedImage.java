2c43e9ca015a5471f9ead2ede9151097f5c6e8dc
Bianca <bianca.blajan@fwfcompany.com>
2018-08-25 16:30:00 +0300

    ADDED ZOOM WITH PINCH

--- MyDocuments/app/src/main/java/me/licenta/mydocuments/DisplayClickedImage.java
+++ MyDocuments/app/src/main/java/me/licenta/mydocuments/DisplayClickedImage.java
@@ -9,9 +9,13 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.github.chrisbanes.photoview.PhotoView;
 
 import java.io.File;
 
@@ -23,14 +27,32 @@ public class DisplayClickedImage extends Activity {
 
         final String imagePath = getIntent().getStringExtra("ClickedImagePath"); //get image path from post activity
         if (imagePath != null) {
-             File imgFile = new File(imagePath);
+            final File imgFile = new File(imagePath);
             if (imgFile.exists()) {
 
-                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                final Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
 
                 ImageView myImage = (ImageView) findViewById(R.id.imageViewDocument);
                 myImage.setImageBitmap(myBitmap);
 
+                //create Alert Dialog when clicked on the image so it can be zoomed
+                myImage.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        new Handler().post(new Runnable() {
+                            @Override
+                            public void run() {
+                                AlertDialog.Builder mBuilder = new AlertDialog.Builder(DisplayClickedImage.this);
+                                View mView = getLayoutInflater().inflate(R.layout.pinch_displayedimage, null);
+                                PhotoView photoView = mView.findViewById(R.id.imageView);
+                                photoView.setImageBitmap(myBitmap);
+                                mBuilder.setView(mView);
+                                AlertDialog mDialog = mBuilder.create();
+                                mDialog.show();
+                            }
+                        });
+                        }
+                });
             }
 
         }
---
+++ MyDocuments/app/src/main/java/me/licenta/mydocuments/Pinch_DisplayedImage.java
@@ -0,0 +1,12 @@
+package me.licenta.mydocuments;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Pinch_DisplayedImage extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pinch_displayedimage);
+    }
+}
